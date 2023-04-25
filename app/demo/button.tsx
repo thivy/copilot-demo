aa57d52302f4bafab2b1605a728857e89988c29f
thivy <thivyakanth@gmail.com>
2023-04-25 15:29:06 +1000

    fix prompting

--- app/demo/button.tsx
+++ app/demo/button.tsx
@@ -4,5 +4,6 @@
 Create and export a ReactJS button component.
 The text of the button is passed as a children prop.
 The button is styled with Tailwind CSS where the background colour teal.
-On hover the button should turn dark teal and the border should be light teal with 2px width
+On hover the button should turn dark teal.
+The button should have an orange colour 2px border size. 
 */
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import Button from "./demo/button";
+
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center pt-10 gap-4">
       <div className="flex items-center flex-col bg-purple-400/20 border-purple-400/30 border p-24 rounded-lg max-w-xl">
         <img src="./Hero image.png" />
         <h1 className="text-2xl font-extrabold py-3">The Prime Numbers</h1>
-        <div className="grid grid-cols-4 gap-4 p-10">
+        <div className="flex flex-wrap gap-4 p-10">
           {/* TODO: Insert the button here */}
+          <Button>test</Button>
         </div>
       </div>
     </main>
