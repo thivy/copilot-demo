98bff4de23c20b8cb6d79c660ab038d5cd6a0ad8
thivy <thivyakanth@gmail.com>
2023-11-30 15:22:00 +1100

    Refactor credit card generation and expiry date functions

--- app/demo/credit-card.ts
+++ app/demo/credit-card.ts
@@ -1,10 +1,31 @@
 const MAX_DIGITS = 16;
 const EXPIRE_YEAR_LENGTH = 3;
 
+/**
+ * Generates a random credit card number.
+ *
+ * @returns {string} The generated credit card number.
+ */
 export const generateRandomCreditCardNumber = () => {
-  return "0000 0000 0000 0000";
+  let creditCardNumber = "";
+  for (let i = 0; i < MAX_DIGITS; i++) {
+    creditCardNumber += Math.floor(Math.random() * 10);
+    if (i % 4 === 3 && i < MAX_DIGITS - 1) {
+      creditCardNumber += " ";
+    }
+  }
+  return creditCardNumber;
 };
 
+/**
+ * Generates a random expiry date for a credit card.
+ * @returns {string} The generated expiry date in the format "MM/YY".
+ */
 export function generateRandomExpiryDate() {
-  return "00/00";
+  const month = Math.floor(Math.random() * 12) + 1;
+  const year = Math.floor(Math.random() * 10) + 20;
+
+  const yearFormat = year.toString().slice(0, EXPIRE_YEAR_LENGTH);
+
+  return `${month}/${yearFormat}`;
 }
