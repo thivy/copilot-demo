7400d0bdf0acf360638b8281c4cd92b6935b669b
thivy <thivyakanth@gmail.com>
2024-01-23 07:09:45 +1100

    Implement functions to generate random credit card number and expiry date

--- app/demo/credit-card.ts
+++ app/demo/credit-card.ts
@@ -1,8 +1,12 @@
 const MAX_DIGITS = 16;
 const EXPIRE_YEAR_LENGTH = 3;
 
+// TODO: Implement the function to generate a random credit card number
 export const generateRandomCreditCardNumber = () => {
   return "1234 5678 9012 3456";
 };
 
-export function generateRandomExpiryDate() {}
+// TODO: Implement the function to generate a random expiry date
+export function generateRandomExpiryDate() {
+  return "12/23";
+}
