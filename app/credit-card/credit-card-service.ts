3b2a858ecccb8a948c1fac73d838292f03648447
thivy <thivyakanth@gmail.com>
2024-03-01 09:11:29 +1100

    Update credit card service to return empty values

--- app/credit-card/credit-card-service.ts
+++ app/credit-card/credit-card-service.ts
@@ -2,9 +2,9 @@ const MAX_DIGITS = 16;
 const EXPIRE_YEAR_LENGTH = 3;
 
 export const getCreditCardNumber = () => {
-  return "1234 5678 9012 3456";
+  return "";
 };
 
 export function generateExpiryDate() {
-  return "TODO:";
+  return "";
 }
