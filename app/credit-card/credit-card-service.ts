f6b91f60f0ff90b7287a276b968071a47f71af07
thivy <thivyakanth@gmail.com>
2024-03-01 13:18:01 +1100

    Update credit card number and expiry date generation

--- app/credit-card/credit-card-service.ts
+++ app/credit-card/credit-card-service.ts
@@ -2,9 +2,9 @@ const MAX_DIGITS = 16;
 const EXPIRE_YEAR_LENGTH = 3;
 
 export const getCreditCardNumber = () => {
-  return "";
+  return "1234 5678 9012 3456";
 };
 
 export function generateExpiryDate() {
-  return "";
+  return "15/25";
 }
