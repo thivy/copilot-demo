86e09be2f484b39c6eba54421a337697993a8078
thivy <thivyakanth@gmail.com>
2024-06-06 09:34:32 +1000

    Refactor Form component and remove console.log statement

--- app/ui/form.tsx
+++ app/ui/form.tsx
@@ -3,7 +3,6 @@
 import { Input } from "./input";
 
 export const Form = () => {
-  console.log("THIS IS FORM");
   return (
     <form className="flex flex-col gap-4">
       <Input placeholder="First Name" />
